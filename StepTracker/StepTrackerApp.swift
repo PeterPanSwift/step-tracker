8233449447b1333ab799477d988e694f258a5558
Peter Pan <apppeterpan@email.ntou.edu.tw>
2024-05-04 22:07:56 +0800

    create project

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by SHIH-YING PAN on 2024/5/4.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
